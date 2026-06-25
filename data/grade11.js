3b9f83891e1d38eb4133a0263c66c489e2b12a43
fhggit <fhernandezgallego8@gmail.com>
2026-06-25 11:45:13 -0500

    Create grade11.js
    Se agregan unidades de onceavo grado: de unidades 1 a 4

---
+++ data/grade11.js
@@ -0,0 +1,44 @@
+const grade11 = {
+
+unit1:{
+title:"ICFES Vocabulary",
+questions:[
+"environment",
+"education",
+"technology",
+"community",
+"opportunity",
+"responsibility"
+]
+},
+
+unit2:{
+title:"Opinions",
+questions:[
+"I agree with you",
+"I disagree",
+"In my opinion",
+"I think that"
+]
+},
+
+unit3:{
+title:"Problem Solving",
+questions:[
+"we should recycle",
+"students must study",
+"people need education"
+]
+},
+
+unit4:{
+title:"Real Life Communication",
+questions:[
+"Can you help me?",
+"Could you repeat that?",
+"What do you mean?",
+"Thank you for your time"
+]
+}
+
+};
