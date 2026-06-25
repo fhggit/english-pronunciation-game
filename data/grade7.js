b165226903278689c5f2aaeb04fc67bd8a8b902f
fhggit <fhernandezgallego8@gmail.com>
2026-06-25 11:40:18 -0500

    Update grade7.js
    Se agregan unidades de séptimo grado: de unidades 3 a 5

--- data/grade7.js
+++ data/grade7.js
@@ -1,8 +1,8 @@
 const grade7 = {
 
-unit1: {
-title: "Countries and Nationalities",
-questions: [
+unit1:{
+title:"Countries and Nationalities",
+questions:[
 "Colombia",
 "Brazil",
 "Mexico",
@@ -12,15 +12,51 @@ questions: [
 ]
 },
 
-unit2: {
-title: "Family Members",
-questions: [
+unit2:{
+title:"Family Members",
+questions:[
 "mother",
 "father",
 "sister",
 "brother",
 "grandmother",
 "grandfather"
+]
+},
+
+unit3:{
+title:"Daily Routines",
+questions:[
+"wake up",
+"have breakfast",
+"go to school",
+"do homework",
+"have dinner",
+"go to bed"
+]
+},
+
+unit4:{
+title:"House Rooms",
+questions:[
+"bedroom",
+"kitchen",
+"living room",
+"bathroom",
+"dining room",
+"garage"
+]
+},
+
+unit5:{
+title:"Food and Drinks",
+questions:[
+"water",
+"juice",
+"milk",
+"bread",
+"rice",
+"chicken"
 ]
 }
 
