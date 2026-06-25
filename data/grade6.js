dafd31007a577f983bc4babe380ef4929bc7c8ba
fhggit <fhernandezgallego8@gmail.com>
2026-06-25 10:37:16 -0500

    Create grade6.js
    Se agregó el contenido de sexto grado

---
+++ data/grade6.js
@@ -0,0 +1,53 @@
+const grade6 = {
+
+unit1: {
+title: "Greetings and Goodbyes",
+questions: [
+"hello",
+"hi",
+"hey",
+"good morning",
+"good afternoon",
+"good night"
+]
+},
+
+unit2: {
+title: "Courtesy Expressions",
+questions: [
+"thank you",
+"please",
+"you are welcome",
+"excuse me",
+"sorry",
+"goodbye",
+"see you later",
+"have a nice day"
+]
+},
+
+unit3: {
+title: "Professions",
+questions: [
+"teacher",
+"student",
+"doctor",
+"artist",
+"engineer",
+"pilot"
+]
+},
+
+unit4: {
+title: "School Objects",
+questions: [
+"book",
+"notebook",
+"pencil",
+"eraser",
+"desk",
+"chair"
+]
+}
+
+};
