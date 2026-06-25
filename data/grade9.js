76421d37134180ded556e69d7b6adceca2707c30
fhggit <fhernandezgallego8@gmail.com>
2026-06-25 12:01:33 -0500

    Create grade9.js
    Se agregan unidades de noveno grado: de unidades 1 a 4

---
+++ data/grade9.js
@@ -0,0 +1,47 @@
+const grade9 = {
+
+unit1:{
+title:"Present Continuous",
+questions:[
+"I am studying English",
+"She is reading a book",
+"They are playing soccer",
+"We are learning today"
+]
+},
+
+unit2:{
+title:"Places in Town",
+questions:[
+"hospital",
+"bank",
+"restaurant",
+"library",
+"supermarket",
+"school"
+]
+},
+
+unit3:{
+title:"Giving Directions",
+questions:[
+"turn left",
+"turn right",
+"go straight ahead",
+"cross the street"
+]
+},
+
+unit4:{
+title:"Technology",
+questions:[
+"computer",
+"internet",
+"smartphone",
+"keyboard",
+"screen",
+"website"
+]
+}
+
+};
