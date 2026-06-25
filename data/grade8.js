dcc74ab9348e3c6d3b9af0694d06275e7e684cb0
fhggit <fhernandezgallego8@gmail.com>
2026-06-25 11:41:30 -0500

    Create grade8.js
    Se agregan unidades de octavo grado: de unidades 1 a 4

---
+++ data/grade8.js
@@ -0,0 +1,46 @@
+const grade8 = {
+
+unit1:{
+title:"Simple Present",
+questions:[
+"I play soccer",
+"She likes music",
+"We study English",
+"They live in Colombia"
+]
+},
+
+unit2:{
+title:"School Subjects",
+questions:[
+"Mathematics",
+"Science",
+"English",
+"History",
+"Art",
+"Physical Education"
+]
+},
+
+unit3:{
+title:"Free Time Activities",
+questions:[
+"play video games",
+"watch television",
+"listen to music",
+"ride a bicycle",
+"read books"
+]
+},
+
+unit4:{
+title:"Weather",
+questions:[
+"it is sunny",
+"it is rainy",
+"it is cloudy",
+"it is windy"
+]
+}
+
+};
