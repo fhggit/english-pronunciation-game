14ddeb57fe6852ad424a4dc2ef1cf5931ef84819
fhggit <fhernandezgallego8@gmail.com>
2026-06-25 11:43:51 -0500

    Create grade10.js
    Se agregan unidades de noveno grado: de unidades 1 a 4

---
+++ data/grade10.js
@@ -0,0 +1,46 @@
+const grade10 = {
+
+unit1:{
+title:"Simple Past",
+questions:[
+"I visited my grandmother",
+"She watched a movie",
+"They played soccer",
+"We studied for the exam"
+]
+},
+
+unit2:{
+title:"Travel",
+questions:[
+"airport",
+"hotel",
+"passport",
+"ticket",
+"tourist",
+"vacation"
+]
+},
+
+unit3:{
+title:"Future Plans",
+questions:[
+"I will travel abroad",
+"I will study engineering",
+"We will learn English"
+]
+},
+
+unit4:{
+title:"Jobs and Careers",
+questions:[
+"doctor",
+"engineer",
+"teacher",
+"architect",
+"lawyer",
+"programmer"
+]
+}
+
+};
